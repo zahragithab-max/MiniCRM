553743896198a0bbe9b1ec9f43670102096eebb9
zahra <zahra@gmail.com>
2026-08-23 01:19:45 -0700

    Fix password hashing in auth service

--- app/Modules/Auth/AuthService.php
+++ app/Modules/Auth/AuthService.php
@@ -3,14 +3,12 @@
 namespace App\Modules\Auth;
 
 use App\Models\User;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 
 class AuthService
 {
     public function register(array $data): User
     {
-        $data['password'] = Hash::make($data['password']);
 
         return User::create($data);
     }
